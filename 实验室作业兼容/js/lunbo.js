bed15534f3c217156ab5cc3a23012cddef0fae1d
Ricky Tan <tan@RickydeMacBook-Pro.local>
2015-12-17 22:13:03 +0800

    兼容IE7

--- 实验室作业兼容/js/lunbo.js
+++ 实验室作业兼容/js/lunbo.js
@@ -11,9 +11,9 @@ window.onload = function () {
 			var last = parseFloat(pic.style.left) + move;
 
 			function flash(){
-				speed = (last-parseFloat(pic.style.left))/(time/interval);
+				speed = (last - parseFloat(pic.style.left))/(time/interval);
 				if (speed < 0) {
-					speed = Math.floor(speed)
+					speed = Math.floor(speed);
 				};
 				if (speed < 0 && parseFloat(pic.style.left) > last) {
                    pic.style.left = parseFloat(pic.style.left) + speed + 'px';
@@ -22,29 +22,28 @@ window.onload = function () {
 				else{
 					wait = false;
 	                pic.style.left = last + 'px';
-                    if (pic.style.left == -1108 + 'px') {
-			        pic.style.left = -277 + 'px';
+                    if (pic.style.left == -831 + 'px') {
+			        pic.style.left = 0 + 'px';
                     }    
 	            }
-	            if (parseFloat(pic.style.left) < -277&&parseFloat(pic.style.left) > -554) {
-	            	li[2].setAttribute('class','pic_active');
-	            	li[1].setAttribute('class','pic_inactive');
-	            	li[3].setAttribute('class','pic_inactive');
+	            if (parseFloat(pic.style.left) < 0&&parseFloat(pic.style.left) > -277) {
+	            	li[2].className = 'pic_active';
+	            	li[1].className = '';
+	            	li[3].className = '';
 
 	            }
-	            else if(parseFloat(pic.style.left) < -554&&parseFloat(pic.style.left) > -831) {
-	            	li[1].setAttribute('class','pic_active');
-	            	li[2].setAttribute('class','pic_inactive');
-	            	li[3].setAttribute('class','pic_inactive');
+	            else if(parseFloat(pic.style.left) < -277&&parseFloat(pic.style.left) > -554) {
+	            	li[1].className = 'pic_active';
+	            	li[2].className = '';
+	            	li[3].className = '';
 	            }
-	            else if((parseFloat(pic.style.left) < -277&&parseFloat(pic.style.left) > 0)||(parseFloat(pic.style.left) < -831)) {
-	            	li[3].setAttribute('class','pic_active');
-	            	li[2].setAttribute('class','pic_inactive');
-	            	li[1].setAttribute('class','pic_inactive');
+	            else if(parseFloat(pic.style.left) < -554) {
+	            	li[3].className = 'pic_active';
+	            	li[2].className = '';
+	            	li[1].className = '';
 	            }
             }
             flash();
-            console.log(3);
 
 		}
 		function startflash(){
@@ -60,10 +59,10 @@ window.onload = function () {
         }
         startflash();
         pic.onmouseover = function() {
-        	stopflash();console.log(1)
+        	stopflash();
         }
         pic.onmouseleave = function(){
-        	startflash();console.log(2)
+        	startflash();
         }
 		// pic.addEventListener("mouseover",function(){stopflash();console.log(1)});
 		//pic.addEventListener("mouseleave",function(){startflash();console.log(2)});
--- 实验室作业兼容/js/way.js
+++ 实验室作业兼容/js/way.js
@@ -1,12 +1,23 @@
-$(document).ready(function(){
-	$(document).on("click",".inactive",function(e){
-		$(".active").attr("class","inactive");
-		$(this).attr("class","active");
-	});
-	$(".ad_float").mouseover(function(){
-		$(this).animate({opacity:1},200)
-	});
-	$(".ad_float").mouseleave(function(){
-		$(this).animate({opacity:0.7},0);
-	});
-});
+(function(){
+	var li = document.getElementById('nav_info').getElementsByTagName('li');
+	for (var i = 0; i < li.length; i++) {
+		li[i].onclick = function(){
+			if (this.className = "inactive") {
+				for (var k = 0; k < li.length; k++) {
+					if (li[k].className = "active") {
+						li[k].className = 'inactive';
+						// class和className兼容方法：
+						// object.setAttribute("class","content")
+						// 在IE8、Chrome、火狐、Opera10中都能设置成功；但是在IE7下无法设置。
+						// object.setAttribute("className","content")
+						// 只有IE7能设置成功，但是其他浏览器均无法设置。
+						// 兼容方法：
+						// 使用 object.className="content"
+					}
+				}
+				this.className = 'active';
+			}
+		}
+	}
+})();
+
